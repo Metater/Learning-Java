c3a720cfc0e8ad2b2b236cc8d8c0e9d32c721d38
Connor <61102529+Metater@users.noreply.github.com>
2021-04-02 18:06:25 -0400

    Hello World

--- src/com/github/metater/Main.java
+++ src/com/github/metater/Main.java
@@ -1,8 +1,11 @@
 package com.github.metater;
 
+import java.lang.System;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    System.out.println("");
     }
 }
