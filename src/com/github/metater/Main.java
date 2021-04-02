e5fc53ca38c40ffb2f341d50711ff13c17867dcc
Connor <61102529+Metater@users.noreply.github.com>
2021-04-02 19:31:29 -0400

    Get url method

--- src/com/github/metater/Main.java
+++ src/com/github/metater/Main.java
@@ -1,11 +1,53 @@
 package com.github.metater;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.System;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    System.out.println("Hola");
+    System.out.println(GetDataFromURL("https://api.coindesk.com/v1/bpi/currentprice.json"));
+    }
+
+    private static String GetDataFromURL(String url)
+    {
+        String data = "";
+        try {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(url);
+            HttpResponse response = httpClient.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                InputStream instream = entity.getContent();
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(instream));
+                    data =  reader.readLine();
+                } catch (IOException ex) {
+                    throw ex;
+                } catch (RuntimeException ex) {
+                    httpGet.abort();
+                    throw ex;
+                } finally {
+                    instream.close();
+                }
+            }
+        } catch (Exception e)
+        {
+            return e.toString();
+        }
+        return data;
     }
 }
